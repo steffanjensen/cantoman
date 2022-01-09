8c2c4acb1838860468d1e26151dac37e0c459628
Steffan Jensen <6hourapp@gmail.com>
2022-01-09 23:33:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -125,7 +125,20 @@ function App() {
   const tokens = data.allTokens;
   tokens.forEach(element => {
    /* const token_url = blockchain.smartContract.methods.tokenURI(element);*/
-    return fetch("https://gateway.pinata.cloud/ipfs/Qmb3AFZR2yrUDB5zMb66M5UUC4A3nhqy6aMgfRVWH8v4nA/" + element + ".json")
+    return fetch("https://gateway.pinata.cloud/ipfs/Qmb3AFZR2yrUDB5zMb66M5UUC4A3nhqy6aMgfRVWH8v4nA/" + element + ".json",{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  }
+                )
     .then((response) => response.json())
     .then((responseJson) => {
      return responseJson.image;
