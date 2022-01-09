e8c1a4c9065279732f013675e6445666cf8774b7
Steffan Jensen <6hourapp@gmail.com>
2022-01-09 23:59:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -131,8 +131,10 @@ function App() {
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
     headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Headers:' '*',
+      'Access-Control-Allow-Methods:' 'POST',
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     redirect: 'follow', // manual, *follow, error
